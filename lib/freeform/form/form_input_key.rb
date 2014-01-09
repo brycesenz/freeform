52553567730ace3f2e16f1f4311444a773c70613
brycesenz <bryce.senz@gmail.com>
2014-01-08 23:10:36 -0500

    Minor form input key changes

--- lib/freeform/form/form_input_key.rb
+++ lib/freeform/form/form_input_key.rb
@@ -7,7 +7,7 @@ module FreeForm
     module ClassMethods
       def form_input_key(name)
         constant = name.to_s.camelize
-        Object.const_set(constant, Class.new) unless Object.const_defined?(constant)    
+        Object.const_set(constant, Module.new) unless const_defined?(constant)    
         define_singleton_method(:model_name) do
           ActiveModel::Name.new(constant.constantize)
         end
