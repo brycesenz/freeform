c53043a2d9150eaf0e38afa6343476253b8a782b
Bryce Senz <bryce.senz@gmail.com>
2014-04-24 10:38:23 -0400

    Changing model save order

--- lib/freeform/form.rb
+++ lib/freeform/form.rb
@@ -32,16 +32,7 @@ module FreeForm
     def save
       return false unless valid? || marked_for_destruction?
 
-      # Loop through nested models first, sending them the save call
-      self.class.models.each do |form_model|
-        model = send(form_model)
-        # This is for nested models.
-        if model.is_a?(Array)
-          model.each { |m| m.save }
-        end
-      end
-
-      # After nested models are handled, save or destroy myself.
+      # Save or destroy myself.
       self.class.models.each do |form_model|
         model = send(form_model)
         # This is for form models.
@@ -51,6 +42,15 @@ module FreeForm
           else
             model.save
           end
+        end
+      end
+
+      # Loop through nested models sending them the save call
+      self.class.models.each do |form_model|
+        model = send(form_model)
+        # This is for nested models.
+        if model.is_a?(Array)
+          model.each { |m| m.save }
         end
       end
     end
@@ -63,65 +63,6 @@ module FreeForm
         model.is_a?(Array) ? model.each { |m| m.save! } : save_or_destroy!(model)
       end
     end
-
-    # def destroy_all_marked_for_destruction
-    #   self.class.models.each do |form_model|
-    #     model = send(form_model)
-    #     # This is for nested form models.
-    #     if model.is_a?(Array)
-    #       model.each { |m| m.destroy_all_marked_for_destruction }
-    #     end
-    #   end
-
-    #   # Destroy objects
-    #   self.class.models.each do |form_model|
-    #     model = send(form_model)
-    #     unless model.is_a?(Array)
-    #       if marked_for_destruction?
-    #         model.destroy
-    #       end
-    #     end
-    #   end
-    # end
-
-    # def save
-    #   return false unless valid? || marked_for_destruction?
-
-    #   # NEW LOGIC!! Destroy all forms from the bottom up, then save all from the top down!
-    #   destroy_all_marked_for_destruction
-
-
-    #   # Save parent form objects first
-    #   self.class.models.each do |form_model|
-    #     model = send(form_model)
-    #     # This is for nested form models.
-    #     unless model.is_a?(Array)
-    #       unless marked_for_destruction?
-    #         # puts "Saving #{model.inspect}"
-    #         model.save
-    #         # puts "Errors = #{model.errors.inspect}"
-    #       end
-    #     end
-    #   end
-
-    #   # Save then call save on nested models
-    #   self.class.models.each do |form_model|
-    #     model = send(form_model)
-    #     # This is for nested form models.
-    #     if model.is_a?(Array)
-    #       model.each { |m| m.save }
-    #     end
-    #   end
-    # end
-
-    # def save!
-    #   raise FreeForm::FormInvalid, "form invalid." unless valid?
-
-    #   self.class.models.each do |form_model|
-    #     model = send(form_model)
-    #     model.is_a?(Array) ? model.each { |m| m.save! } : save_or_destroy!(model)
-    #   end
-    # end
 
   private
     def initialize_child_models
