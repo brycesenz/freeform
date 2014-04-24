3e8bbdc50f26f44f6654f48ed664260dc9f51055
Bryce Senz <bryce.senz@gmail.com>
2014-04-24 11:38:12 -0400

    Adding more debug code

--- lib/freeform/form.rb
+++ lib/freeform/form.rb
@@ -35,13 +35,16 @@ module FreeForm
       # Save or destroy myself.
       self.class.models.each do |form_model|
         model = send(form_model)
-        puts "Model = #{model.inspect}"
+        puts "Model = #{model.to_s}"
         # This is for form models.
         unless model.is_a?(Array)
           if marked_for_destruction?
             model.destroy
           else
-            model.save
+            if model.save
+            else
+              puts "Save errors = #{model.errors.inspect}"
+            end
           end
         end
       end
@@ -50,7 +53,6 @@ module FreeForm
       self.class.models.each do |form_model|
         model = send(form_model)
         # This is for nested models.
-        puts "Nested Model = #{model.inspect}"
         if model.is_a?(Array)
           model.each { |m| m.save }
         end
